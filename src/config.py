a9000726f3e5c972015348ca70c22204009decff
bob joe <matthew@krupczak.org>
2022-12-18 00:40:10 -0500

    update version number to v0.1.30-pre-alpha

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ config.py
 a simple file for storing global variables across all OpenAthena modes
 """
 # software version
-version = "OpenAthena v0.1.29-pre-alpha\nLGPL 2.1 + LICENSE ADDENDUM, Some rights reserved"
+version = "OpenAthena v0.1.30-pre-alpha\nLGPL 2.1 + LICENSE ADDENDUM, Some rights reserved"
 # vector length for each step of getTarget terrain match search (altering may effect accuracy)
 # default: 1.0
 increment = 1.0
